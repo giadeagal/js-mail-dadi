4b40b4068177a6243a7b8b93089fc0ef86c3fa9a
Gianluca <gl.galassini@gmail.com>
2021-05-28 16:26:20 +0200

    correzione h

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var player =  Math.floor(Math.random() * 6) + 1;
 var pc = Math.floor(Math.random() * 6) + 1;
 
-document.getElementById("printer").innerHTML = "hai vinto!"
+document.getElementById("printer").innerHTML = "Hai vinto!"
 
 if (player == pc) {
     document.getElementById("printer").innerHTML = "Siamo pari";
